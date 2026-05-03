86634c81379f85e89586099510a2de8cd1487c6b
sujithkalakonda-techolution <sujith.kalakonda@techolution.com>
2026-05-04 02:09:49 +0530

    Add generate endpoint to API for text generation

---
+++ src/api/routes.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Request
+from src.schemas.requests import GenerateRequest, GenerateResponse
+import time
+
+
+
+router = APIRouter()
+
+# Generate endpoint
+@router.post("/generate")
+def generate(request: Request, body: GenerateRequest):
+    prompt = body.prompt
+    max_tokens = body.max_tokens
+    temperature = body.temperature
+
+    engine = request.app.state.engine
+
+    start_time = time.time()
+
+    # call the engine generate
+    output = engine.generate(prompt, max_tokens, temperature)
+
+    latency_ms = (time.time() - start_time) * 1000
+    tokens_per_sec = output.get("completion_tokens") / (latency_ms / 1000)
+
+    return GenerateResponse(
+        text=output.get("output"),
+        latency_ms=latency_ms,
+        prompt_tokens=output.get("prompt_tokens"),
+        completion_tokens=output.get("completion_tokens"),
+        tokens_per_sec = tokens_per_sec
+    )
+
+
+    
+
+    
+
