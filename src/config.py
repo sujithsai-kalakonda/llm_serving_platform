35b38f8c14706e4e77844facdd7b2535db60d616
sujithkalakonda-techolution <sujith.kalakonda@techolution.com>
2026-05-08 00:28:10 +0530

    Update default model name in settings and remove device configuration, because its handled automatically in hf_engine, and for vllm_engine we definitely require gpu

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings
+import torch
 
 class Settings(BaseSettings):
-    model_name: str = "HuggingFaceTB/SmolLM2-360M-Instruct" # "gpt2" # "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-    device: str = "cuda"
+    model_name: str = "gpt2" # "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
     max_tokens: int = 256
     temperature: float = 0.5
     engine_type: str = "vllm" # hf or vllm
